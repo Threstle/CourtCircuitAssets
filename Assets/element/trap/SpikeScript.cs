5b345faaf1a5e15832f192d6513e377384cfb86e
System Administrator <root@p-212b-01.intra.gobelins.fr>
2015-02-19 17:52:14 +0100

    Spike trap element

---
+++ Assets/element/trap/SpikeScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeScript : MonoBehaviour {
+
+	public float
+		x,
+		y,
+		z,
+		width,
+		height,
+		angle ;
+
+	// Use this for initialization
+	void Start ()
+	{
+		transform.localScale = new Vector3(1, 0, 1) ;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float h = (float) (transform.localScale.y + 0.1);
+		if (h > height) h = height ;
+		transform.localScale = new Vector3(width, h, 1);
+		transform.position = new Vector3(x, y - ((height-h)/4), z);
+	}
+}
