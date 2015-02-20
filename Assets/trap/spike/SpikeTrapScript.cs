624b2fc38bd35043375efe25e7ee2d8ec6f97748
System Administrator <root@p-212b-01.intra.gobelins.fr>
2015-02-20 17:57:05 +0100

    Modified structure

--- Assets/trap/spike/SpikeTrapScript.cs
+++ Assets/trap/spike/SpikeTrapScript.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class SpikeGroupScript : MonoBehaviour {
+public class SpikeTrapScript : MonoBehaviour {
 
     public GameObject reference ;
     public GameObject spike ;
