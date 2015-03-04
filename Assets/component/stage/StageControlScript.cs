8b8a0cec22a95048ab0274243475f9f5ea26fc1a
System Administrator <root@p-212b-01.intra.gobelins.fr>
2015-03-04 18:33:47 +0100

    Added Stage

---
+++ Assets/component/stage/StageControlScript.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StageControlScript : MonoBehaviour
+{
+	public GameObject stageModel ;
+	
+	public List<GameObject> stages ;
+
+	public List<Sprite> backgrounds ;
+
+	void Start ()
+	{
+		Create ();
+		Create ();
+		Create ();
+		Create ();
+		Create ();
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	////////////////////////////////////////////
+
+	void Create()
+	{
+		GameObject stage = Instantiate(stageModel) as GameObject;
+		StageScript stageScript = stage.GetComponent<StageScript>();
+
+		stageScript.id = stages.Count;
+		stage.GetComponent<SpriteRenderer> ().sprite = backgrounds[(int) (Random.Range(0, backgrounds.Count)+0.5)];
+		stage.transform.position = new Vector3(stageScript.id * 8, 0, 0);
+
+		stages.Add (stage);
+		stage.transform.parent = this.transform;
+	}
+}
---
+++ Assets/component/stage/StageScript.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StageScript : MonoBehaviour
+{
+	public int id;
+
+	public float width;
+	public float height;
+	
+	public List<Object> elements ;
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+	
+	}
+}
